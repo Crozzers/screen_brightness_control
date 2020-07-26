c29684bcb2c340dfec13b21bf3630c114b953dfb
Matthew Crossley <matthew.crossley.2003@gmail.com>
2020-07-26 23:53:16 +0100

    Fixed setup.py dependencies for Windows

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
-from setuptools import setup, find_packages
-import platform
-requires=[]
-if platform.system()=='Windows':
-    requires+=['wmi']
+from setuptools import setup
+
 setup(name='screen_brightness_control',
-    version='0.1.4',
+    version='0.1.5',
     url='https://github.com/Crozzers/screen-brightness-control',
     license='MIT',
     author='Crozzers',
     author_email='captaincrozzers@gmail.com',
     packages=['screen_brightness_control'],
-    install_requires=requires,
+    install_requires=['wmi ; platform_system=="Windows"'],
     description='A Python tool to control monitor brightness on Windows and Linux',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
