099cc29ed1bcdbf17813ef429b1bfb87a83b47c9
Crozzers <matthew.crossley.2003@gmail.com>
2020-11-09 18:52:28 +0000

    Added display and verbose command line args

--- screen_brightness_control/__main__.py
+++ screen_brightness_control/__main__.py
@@ -3,18 +3,25 @@ import screen_brightness_control as SBC
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--display', type=int, help='the display to be used')
     parser.add_argument('-s', '--set', type=int, help='set the brightness to this value')
     parser.add_argument('-g', '--get', action='store_true', help='get the current screen brightness')
     parser.add_argument('-f', '--fade', type=int, help='fade the brightness to this value')
+    parser.add_argument('-v', '--verbose', action='store_true', help='any error messages will be more detailed')
 
     args = parser.parse_args()
+    kw = {}
+    if args.display!=None:
+        kw['display'] = args.display
+    if args.verbose:
+        kw['verbose_error']=True
 
     if args.get:
-        print(SBC.get_brightness())
+        print(SBC.get_brightness(**kw))
     elif args.set!=None:
-        SBC.set_brightness(args.set)
+        SBC.set_brightness(args.set, **kw)
     elif args.fade!=None:
-        SBC.fade_brightness(args.fade)
+        SBC.fade_brightness(args.fade, **kw)
     else:
         print("No valid arguments")
 
