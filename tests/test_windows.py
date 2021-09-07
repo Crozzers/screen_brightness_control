3deb75c725e6e2b8e5b40e60e6460b9957447d75
Crozzers <captaincrozzers@gmail.com>
2021-09-07 15:42:53 +0100

    Change test structure to make debugging error messages easier.
    Previously, a single class would test both methods. If something went wrong the error would say something like
    "error in class 'TestAllMethodsBasic'..." and it was hard to find out which method, `WMI` or `VCP`, the error was
    coming from.
    
    Now there is a generic `BasicMethodTest` class that provides the generic tests that `TestWMI` and `TestVCP` inherit.

--- tests/test_windows.py
+++ tests/test_windows.py
@@ -14,23 +14,10 @@ def get_method_names():
     return (i.__name__.lower() for i in get_methods())
 
 
-class TestCase(unittest.TestCase):
-    def assertBrightnessEqual(self, a, b, method):
-        try:
-            self.assertEqual(a, b)
-        except AssertionError as e:
-            if method == windows.WMI:
-                # some laptop displays will only have a set number of levels
-                # the brightness can be set to so check the value is at least close.
-                # this allows for 16ish brightness levels
-                self.assertTrue(abs(a - b) < 7)
-            else:
-                raise e
-
-
 class TestGetDisplayInfo(unittest.TestCase):
     def test_normal(self):
         displays = windows.get_display_info()
+        self.assertIsInstance(displays, list)
         all_methods = get_methods()
 
         for display in displays:
@@ -44,65 +31,69 @@ class TestGetDisplayInfo(unittest.TestCase):
             self.assertIn('method', display)
             self.assertIn(display['method'], all_methods)
 
-    def test_types(self):
-        displays = windows.get_display_info()
-        self.assertIsInstance(displays, list)
-        self.assertTrue(all(type(i) == dict for i in displays))
 
+class BasicMethodTest(object):
+    def assertBrightnessEqual(self, a, b, method):
+        try:
+            self.assertEqual(a, b)
+        except AssertionError as e:
+            if method == windows.WMI:
+                # some laptop displays will only have a set number of levels
+                # the brightness can be set to so check the value is at least close.
+                # this allows for 16ish brightness levels
+                self.assertTrue(abs(a - b) < 7)
+            else:
+                raise e
 
-class TestAllMethodsBasic(TestCase):
-    '''
-    Tests the basics of the `WMI` and `VCP` classes.
-    For more specialized testing of the `VCP` class, see the
-    `TestVCP` class
-    '''
     def test_get_display_info(self):
-        for method in get_methods():
-            displays = method.get_display_info()
-            self.assertIsInstance(displays, list)
-            for display in displays:
-                # since this info is filtered from `windows.get_display_info`
-                # don't do much thorough tests here as we already do
-                # in `TestGetDisplayInfo`
-                self.assertIsInstance(display, dict)
-                self.assertEqual(display['method'], method)
+        displays = self.method.get_display_info()
+        self.assertIsInstance(displays, list)
+        for display in displays:
+            # since this info is filtered from `windows.get_display_info`
+            # don't do much thorough tests here as we already do
+            # in `TestGetDisplayInfo`
+            self.assertIsInstance(display, dict)
+            self.assertEqual(display['method'], self.method)
 
     def test_get_brightness(self):
-        for method in get_methods():
-            displays = method.get_display_info()
-            if displays:  # if there are any displays this method can address
-                brightness = method.get_brightness()
-                self.assertIsInstance(brightness, list)
-                self.assertEqual(len(displays), len(brightness))
+        displays = self.method.get_display_info()
+        if displays:  # if there are any displays this method can address
+            brightness = self.method.get_brightness()
+            self.assertIsInstance(brightness, list)
+            self.assertEqual(len(displays), len(brightness))
 
-                for value in brightness:
-                    self.assertIsInstance(value, int)
-                    self.assertTrue(0 <= value <= 100)
+            for value in brightness:
+                self.assertIsInstance(value, int)
+                self.assertTrue(0 <= value <= 100)
 
-                single_brightness = method.get_brightness(display=0)
-                self.assertIsInstance(single_brightness, list)
-                self.assertEqual(len(single_brightness), 1)
+            single_brightness = self.method.get_brightness(display=0)
+            self.assertIsInstance(single_brightness, list)
+            self.assertEqual(len(single_brightness), 1)
 
     def test_set_brightness(self):
-        for method in get_methods():
-            displays = method.get_display_info()
-            if displays:  # if there are any displays this method can address
-                for i in (0, 10, 25, 45, 50, 60, 71, 80, 99, 100):
-                    self.assertIsNone(method.set_brightness(i))
+        if self.method.get_display_info():  # if there are any displays this method can address
+            for i in (0, 10, 25, 45, 50, 60, 71, 80, 99, 100):
+                self.assertIsNone(self.method.set_brightness(i))
 
-                    for value in method.get_brightness():
-                        self.assertIsInstance(value, int)
-                        self.assertBrightnessEqual(value, i, method)
+                for value in self.method.get_brightness():
+                    self.assertIsInstance(value, int)
+                    self.assertBrightnessEqual(value, i, self.method)
 
 
-class TestVCP(unittest.TestCase):
+class TestWMI(BasicMethodTest, unittest.TestCase):
+    method = windows.WMI
+
+
+class TestVCP(BasicMethodTest, unittest.TestCase):
     '''More specialized tests for `windows.VCP` methods not covered by `TestAllMethodsBasic`'''
+    method = windows.VCP
+
     def test_iter_physical_monitors(self):
-        monitors = list(windows.VCP.iter_physical_monitors())
-        self.assertEqual(len(monitors), len(windows.VCP.get_display_info()))
+        monitors = list(self.method.iter_physical_monitors())
+        self.assertEqual(len(monitors), len(self.method.get_display_info()))
 
         # check that the start kwarg does indeed skip handles correctly
-        self.assertGreater(len(monitors), len(list(windows.VCP.iter_physical_monitors(start=1))))
+        self.assertGreater(len(monitors), len(list(self.method.iter_physical_monitors(start=1))))
 
 
 class TestListMonitorsInfo(unittest.TestCase):
