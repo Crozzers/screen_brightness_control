9ebf95bf30a8576c7e39f07c5ab6b3cb06ee2a6e
Crozzers <captaincrozzers@gmail.com>
2022-02-04 11:59:26 +0000

    Fix setup.py not working if Windows dependencies not installed.
    Previously it would import `__version__` from the module itself, which would fail on Windows if WMI wasn't installed.
    Now the version and author info has been moved to `_version.py`.

---
+++ screen_brightness_control/_version.py
@@ -0,0 +1,4 @@
+'''Short file to provide version and author information for this package'''
+
+__version__ = '0.11.3'
+__author__ = 'Crozzers'
--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-import os
 import sys
 
 from setuptools import setup
 
-sys.path.insert(0, os.getcwd())
-from screen_brightness_control import __version__  # noqa: E402
+sys.path.insert(0, 'screen_brightness_control')
+from _version import __author__, __version__  # noqa: E402
 
 setup(
     name='screen_brightness_control',
@@ -15,7 +14,7 @@ setup(
         'Issue Tracker': 'https://github.com/Crozzers/screen_brightness_control/issues'
     },
     license='MIT',
-    author='Crozzers',
+    author=__author__,
     author_email='captaincrozzers@gmail.com',
     packages=['screen_brightness_control'],
     install_requires=['wmi ; platform_system=="Windows"'],
