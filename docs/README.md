18a0635a821e1cc0e8610f4bd421fb7c6e6b6f5f
Crozzers <captaincrozzers@gmail.com>
2022-02-06 19:13:50 +0000

    Add script to generate entire Github pages repository.
    Generates docs for v0.5.1 and up

--- docs/README.md
+++ docs/README.md
@@ -5,7 +5,18 @@ This directory contains the tools used to generate this [project's documentation
 
 ## Build instructions
 
+To build documentation for the current working tree (assuming same directory layout as git repo):
 ```
 pip install pdoc
 python docs/make.py
 ```
+
+To build documentation for "all versions" of this project (assuming git tags are present):
+```
+pip install pdoc
+python docs/build_full_documentation.py
+```
+By "all versions" I mean from v0.5.1 and up because that is the first version with docstrings that enable documentation.
+
+---
+The resulting documentation from both of these scripts will be located in `docs/docs/`
---
+++ docs/build_full_documentation.py
@@ -0,0 +1,42 @@
+#!/bin/python
+# flake8: noqa: E501
+
+import os
+import subprocess
+import tarfile
+from pathlib import Path
+import shutil
+import sys
+
+from packaging import version
+
+here = Path(__file__).parent
+temp = here / 'temp'
+os.makedirs(temp, exist_ok=True)
+
+# get list of all versions from git tag
+all_versions = subprocess.check_output(['git', 'tag', '-l']).decode().rstrip('\n').split('\n')
+
+# download any versions after v0.5.0
+for v in filter(lambda a: version.Version(a) > version.Version('0.5.0'), all_versions):
+    v = version.Version(v.replace('v', ''))
+    if not os.path.isfile(temp / f'screen_brightness_control-{v}.tar.gz'):
+        subprocess.check_output(['pip', 'download', '--no-deps', '--no-binary', ':all:', '-d', str(temp), f'screen_brightness_control=={v}'])
+
+for file in os.listdir(temp):
+    if os.path.isdir(temp / file):
+        continue
+
+    # extract each downloaded version
+    with tarfile.open(temp / file) as t:
+        out = temp / file.replace('screen_brightness_control-', '').replace('.tar.gz', '')
+        if os.path.isdir(out):
+            shutil.rmtree(out)
+        t.extractall(out)
+
+        # now find the __init__.py file in there and generate the docs for that module
+        for root, _, files in os.walk(out):
+            for f in files:
+                if f == '__init__.py':
+                    subprocess.check_output([sys.executable, str(here / 'make.py'), '-p', root])
+                    break
