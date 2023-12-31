137d86636c7d7a6d681bb74234e2f371d402f0b4
Crozzers <captaincrozzers@gmail.com>
2023-12-31 12:05:06 +0000

    Make generic class for brightness function tests
    This should cover common functionality between `get/set/fade_brightness`

--- pytests/helpers.py
+++ pytests/helpers.py
@@ -1,5 +1,5 @@
-from abc import ABC, abstractmethod
-from typing import Any, Dict, List, Optional, Type
+from abc import ABC
+from typing import Any, Callable, Dict, List, Literal, Optional, Tuple, Type, Union
 from unittest.mock import Mock
 import pytest
 
@@ -146,3 +146,119 @@ class BrightnessMethodTest(ABC):
         def test_returns_nothing(self, method: Type[BrightnessMethod]):
             assert method.set_brightness(100) is None
             assert method.set_brightness(100, display=0) is None
+
+
+# some types for `BrightnessFunctionTest`, outside the class body so that subclasses can access
+BFOpType = Literal['get', 'set', 'fade']
+'''Brightness function operation type'''
+BFPatchType = Dict[BrightnessMethod, Dict[BFOpType, Mock]]
+'''Brightness function patch type'''
+
+class BrightnessFunctionTest(ABC):
+    @pytest.fixture(autouse=True)
+    def patch_methods(self, mocker: MockerFixture, mock_os_module) -> BFPatchType:
+        '''
+        Patch brightness getters to return display index, patch brightness setters to do nothing
+
+        Returns:
+            dict of methods and their mocks
+        '''
+        def side_effect(display=None):
+            return [display]
+
+        def do_nothing(value, display=None):
+            return None
+
+        displays = mock_os_module.list_monitors_info()
+        method_patches = {}
+        for display in displays:
+            method = display['method']
+            if method in method_patches:
+                continue
+            method_patches[method] = {
+                'set': mocker.patch.object(method, 'set_brightness', Mock(side_effect=do_nothing)),
+                'get': mocker.patch.object(method, 'get_brightness', Mock(side_effect=side_effect))
+            }
+        return method_patches
+
+    @pytest.fixture
+    def operation_type(self) -> BFOpType:
+        raise NotImplementedError
+
+    @pytest.fixture
+    def operation(self, operation_type: BFOpType) -> Tuple[Callable, Tuple, bool]:
+        '''
+        Returns:
+            The current brightness function under test, arguments to pass to said function, and whether
+            that function should return None
+        '''
+        args = (100,)
+        returns_none = False
+        if operation_type == 'set':
+            func = sbc.set_brightness
+            returns_none = True
+        elif operation_type == 'get':
+            func = sbc.get_brightness
+            args = ()
+        else:  # fade
+            func = sbc.fade_brightness
+
+        return (func, args, returns_none)
+
+    class TestDisplayKwarg:
+        @pytest.mark.parametrize('identifier', ['index', 'name', 'serial', 'edid'])
+        def test_identifiers(self, patch_methods: BFPatchType, operation_type: BFOpType, operation, displays, identifier: str):
+            '''Test referencing a display by a `DisplayIdentifier` works'''
+            for index, display in enumerate(displays):
+                spy = patch_methods[display['method']][operation_type]
+
+                func, args, returns_none = operation
+
+                # added `type: ignore` because vscode was falsely raising errors for `args`
+                # have to call with global index
+                result = func(*args, display=index if identifier == 'index' else display[identifier])  # type: ignore
+                # filter_monitors resolves the identifier away to the index
+                # so just check that we call the right method with the right index
+                if operation_type == 'fade':
+                    # must be called with final value at least once
+                    spy.assert_any_call(*args, display=display['index'])
+                    for call in spy.mock_calls:
+                        # every call must be to the correct display
+                        assert call.kwargs == {'display': display['index']}
+                else:
+                    spy.assert_called_once_with(*args, display=display['index'])
+
+                assert result == (None if returns_none else [display['index']])
+
+                spy.reset_mock()
+
+        def test_none(self, patch_methods: BFPatchType, operation_type: BFOpType, operation, displays):
+            '''Test all displays are fetched if no kwarg given'''
+            func, args, _ = operation
+
+            func(*args, display=None)
+
+            # check all methods called for all displays
+            for display in displays:
+                patch_methods[display['method']][operation_type].assert_any_call(*args, display=display['index'])
+
+    class TestMethodKwarg:
+        def test_none(self, patch_methods: BFPatchType, operation_type: BFOpType, operation):
+            '''Test all methods get called if no method kwarg given'''
+            func, args, _ = operation
+            func(*args)
+            for method in patch_methods.values():
+                method[operation_type].assert_called()
+
+        def test_methods(self, patch_methods: BFPatchType, operation_type: BFOpType, operation):
+            '''Test method kwarg ensures only that method is called'''
+            func, args, _ = operation
+            for method, spies in patch_methods.items():
+                spy = spies[operation_type]
+                func(*args, method=method.__name__)
+                spy.assert_called()
+                for other_method, other_spies in patch_methods.items():
+                    if other_method is method:
+                        continue
+                    other_spies[operation_type].assert_not_called()
+                spy.reset_mock()
--- pytests/test_init.py
+++ pytests/test_init.py
@@ -1,34 +1,17 @@
-from typing import Any, Dict, Tuple, Type
+from typing import Any, Callable, Dict, List, Literal, Optional, Tuple, Type
 from unittest.mock import Mock, call
 
 import pytest
 from pytest_mock import MockerFixture
+from .helpers import BrightnessFunctionTest
 
 import screen_brightness_control as sbc
 
 
-class TestGetBrightness:
+class TestGetBrightness(BrightnessFunctionTest):
     @pytest.fixture
-    def patch_methods(
-        self, mocker: MockerFixture, mock_os_module
-    ) -> Dict[Type[sbc.helpers.BrightnessMethod], Mock]:
-        '''
-        Patch all brightness methods to return the display's index
-
-        Returns:
-            dict of methods and their mocks
-        '''
-        def side_effect(display=None):
-            return [display]
-
-        displays = mock_os_module.list_monitors_info()
-        method_patches = {}
-        for display in displays:
-            method = display['method']
-            if method in method_patches:
-                continue
-            method_patches[method] = mocker.patch.object(method, 'get_brightness', Mock(side_effect=side_effect))
-        return method_patches
+    def operation_type(self):
+        return 'get'
 
     def test_returns_list_of_int(self):
         '''Check return types and integer bounds'''
@@ -37,75 +20,10 @@ class TestGetBrightness:
         assert all(isinstance(i, int) for i in brightness)
         assert all(0 <= i <= 100 for i in brightness)  # type: ignore
 
-    class TestDisplayKwarg:
-        @pytest.mark.parametrize('identifier', ['index', 'name', 'serial', 'edid'])
-        def test_identifiers(self, patch_methods: Dict[Any, Mock], displays, identifier: str):
-            '''Test referencing a display by a `DisplayIdentifier` works'''
-            for index, display in enumerate(displays):
-                spy = patch_methods[display['method']]
-                # have to call with global index
-                result = sbc.get_brightness(display=index if identifier == 'index' else display[identifier])
-                # filter_monitors resolves the identifier away to the index
-                # so just check that we call the right method with the right index
-                spy.assert_called_once_with(display=display['index'])
-                assert result == [display['index']]
-
-                spy.reset_mock()
-
-        def test_none(self, patch_methods: Dict[Any, Mock], displays):
-            '''Test all displays are fetched if no kwarg given'''
-            sbc.get_brightness(display=None)
-            # check all methods called for all displays
-            for display in displays:
-                patch_methods[display['method']].assert_any_call(display=display['index'])
-
-    class TestMethodKwarg:
-        def test_none(self, patch_methods: Dict[Any, Mock]):
-            '''Test all methods get called if no method kwarg given'''
-            sbc.get_brightness()
-            for method in patch_methods.values():
-                method.assert_called()
-
-        def test_methods(self, patch_methods: Dict[Any, Mock]):
-            '''Test method kwarg ensures only that method is called'''
-            for method, spy in patch_methods.items():
-                sbc.get_brightness(method=method.__name__)
-                spy.assert_called()
-                for other_method, other_spy in patch_methods.items():
-                    if other_method is method:
-                        continue
-                    other_spy.assert_not_called()
-                spy.reset_mock()
-
-
-class TestSetBrightness:
-    @pytest.fixture(autouse=True)
-    def patch_methods(
-        self, mocker: MockerFixture, mock_os_module
-    ) -> Dict[Type[sbc.helpers.BrightnessMethod], Tuple[Mock]]:
-        '''
-        Patch brightness getters to return display index, patch brightness setters to do nothing
-
-        Returns:
-            dict of methods and their mocks
-        '''
-        def side_effect(display=None):
-            return [display]
-
-        def do_nothing(value, display=None):
-            return None
-
-        displays = mock_os_module.list_monitors_info()
-        method_patches = {}
-        for display in displays:
-            method = display['method']
-            if method in method_patches:
-                continue
-            method_patches[method] = (
-                mocker.patch.object(method, 'get_brightness', Mock(side_effect=side_effect)),
-                mocker.patch.object(method, 'set_brightness', Mock(side_effect=do_nothing))
-            )
-        return method_patches
+class TestSetBrightness(BrightnessFunctionTest):
+    @pytest.fixture
+    def operation_type(self):
+        return 'set'
 
     def test_returns_none_by_default(self):
         assert sbc.set_brightness(100) is None
