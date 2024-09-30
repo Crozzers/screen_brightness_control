d9151092cd49385521c633751b043aa62407cbe8
BingoWon <bingow@outlook.com>
2024-10-01 00:12:57 +0800

    fix issues from pytest

--- tests/mocks/helpers_mock.py
+++ tests/mocks/helpers_mock.py
@@ -14,6 +14,7 @@ class MockBrightnessMethod(BrightnessMethod):
             'serial': 'serial1',
             'edid': '00ffffffffff00edid1',
             'method': cls,
+            'uid': '1000',
             'index': 0
         }]
 
